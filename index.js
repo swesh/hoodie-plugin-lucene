43a9ad5a5d680a386aa10cb49f2a48cb0cff59cd
Dale Shaw <dale@xiatron.com>
2015-01-27 16:16:37 -0700

    Added basic flow.

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
 /*
- * Copyright 2015 Xiatron LLC
- */
+*   Copyright 2015 Xiatron LLC
+*/
+
+// Set some vars
+var ports = require('ports');
+var appName = require('../../package.json').name;
+var port = ports.getPort(appName+'-hoodie-plugin-lucene');
+var fs = require('fs');
+var lucenePath = '/srv/couchdb-lucene';
+var status = 'Lucene Plugin is running'; //default status
 
 // Run in the hoodie context
 module.exports = function (hoodie, cb) {
-    // Output something useful
-    console.log('Lucene Plugin is running');
+    // SET THE PROXY HANDLER
+    var value = '{couch_httpd_proxy, handle_proxy_req, <<"http://127.0.0.1:'+port+'">>}';
+    hoodie.request('PUT', '_config/httpd_global_handlers/_fti/', {data:JSON.stringify(value)},function(err, data){
+        if (err) console.log(err);
+    });
     
-    //Hoodie Callback
+    /*
+    *   SETUP THE LUCENE INSTANCE
+    *   Assumes Lucene has already been
+    *   installed to /srv/couchdb-lucene
+    *   and that the files are accessible
+    */
+    fs.exists(lucenePath, function(exists) {
+        if (exists) {
+            // Copy the files to the app
+        } else {
+            // Notify
+            status = 'Lucene is missing.  Please install to '+lucenePath+' and restart the app.';
+        }
+    });
+
+    // Output something useful
+    console.log(status);
+    
+    // Hoodie Callback
     cb();
 }
