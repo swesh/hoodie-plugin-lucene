930c6aa8bac952c381d7502d137703850af9a83d
Dale Shaw <dale@xiatron.com>
2015-01-27 14:58:18 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,2 @@
+# Lucene Plugin for Hoodie
+This plugin adds lucene indexing to a hoodie app.  It is currently under development by Appback.com and will be provided as part of the [Appback BaaS](https://appback.com).
---
+++ index.js
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2015 Xiatron LLC
+ */
+
+// Run in the hoodie context
+module.exports = function (hoodie, cb) {
+    // Output something useful
+    console.log('Lucene Plugin is running');
+    
+    //Hoodie Callback
+    cb();
+}
