dc9fdf37931e44f4a9285517e1d4b8755ca9af0a
Dale Shaw <dale@xiatron.com>
2015-01-28 23:29:29 -0700

    Fixed configItems object.

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ var spawn = require('child_process').spawn;
 var lucenePath = '/srv/couchdb-lucene';
 var luceneProcess;
 var configItems = [
-    {"couchdb/os_process_timeout/":"60000"},
-    {"external/_fti/":"/usr/bin/python \"/home/hoodiehost/apps/test-011715a/data/couchdb-lucene/tools/couchdb-external-hook.py --remote-port "+port+"\""},
-    {"httpd_global_handlers/_fti/":"{couch_httpd_external, handle_external_req, <<\"fti\">>}"}
+    ['couchdb/os_process_timeout/','60000'],
+    ['external/fti/','/usr/bin/python "/home/hoodiehost/apps/test-011715a/data/couchdb-lucene/tools/couchdb-external-hook.py --remote-port '+port+'"'],
+    ['httpd_db_handlers/_fti/','{couch_httpd_external, handle_external_req, <<"fti">>}']
 ];
 
 
